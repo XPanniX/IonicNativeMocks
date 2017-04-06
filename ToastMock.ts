fdc73666c1be9cc04f96f5d506fd08070c6893b1
Jan Wennrich <jan@pcsg.de>
2017-04-06 14:28:46 +0200

    Added Toast Mock

--- ToastMock.ts
+++ ToastMock.ts
@@ -1,12 +1,41 @@
-import {Clipboard} from "@ionic-native/clipboard";
+import {Toast, ToastOptions} from "@ionic-native/toast";
+import {Observable} from "rxjs";
 
-export class ClipboardMock extends Clipboard {
+export class ToastMock extends Toast {
 
-    copy(text: string): Promise<any> {
+    show(message: string, duration: string, position: string): Observable<any> {
+        return Observable.empty();
+    }
+
+    hide(): Promise<any> {
         return new Promise(() => {});
     }
 
-    paste(): Promise<any> {
-        return new Promise(() => {});
+    showWithOptions(options: ToastOptions): Observable<any> {
+        return Observable.empty();
+    }
+
+    showShortTop(message: string): Observable<any> {
+        return Observable.empty();
+    }
+
+    showShortCenter(message: string): Observable<any> {
+        return Observable.empty();
+    }
+
+    showShortBottom(message: string): Observable<any> {
+        return Observable.empty();
+    }
+
+    showLongTop(message: string): Observable<any> {
+        return Observable.empty();
+    }
+
+    showLongCenter(message: string): Observable<any> {
+        return Observable.empty();
+    }
+
+    showLongBottom(message: string): Observable<any> {
+        return Observable.empty();
     }
 }
